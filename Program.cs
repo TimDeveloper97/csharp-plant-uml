e0c3e233b2fd0c9675a3418f0f95f44b32054e5b
TimDeveloper97 <timdeveloper97@gmail.com>
2024-03-22 22:00:47 +0700

    update change source and target

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class State
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public List<Transition> Transitions { get; set; }
-
+    public string Type { get; set; }
     public int ParrentId { get; set; }
 
-    public State(int id, string name, int parrentId)
+    public State(int id, string name, string type, int parrentId)
     {
         Id = id;
         ParrentId = parrentId;
+        Type = type;
         Name = name;
         Transitions = new List<Transition>();
     }
@@ -54,7 +56,7 @@ public class PlantUmlAnalyzer
                     var sta = FindStateByName(states, stateName, qParrent.Peek());
                     if(sta == null)
                     {
-                        sta = new State(++id, stateName, qParrent.Peek());
+                        sta = new State(++id, stateName, "State", qParrent.Peek());
 
                         states.Add(sta);
                         qParrent.Push(id);
@@ -68,27 +70,36 @@ public class PlantUmlAnalyzer
                     if(qParrent.Count > 1)
                         qParrent.Pop();
                 }
-                else if (trimmedLine.Contains("-->"))
+                else if (trimmedLine.Contains("-->") 
+                    || trimmedLine.Contains("->"))
                 {
-                    string[] parts = trimmedLine.Split(new[] { "-->" }, StringSplitOptions.None);
+                    string[] parts = trimmedLine.Split(new[] { "-->", "->" }, StringSplitOptions.None);
                     string sourceStateName = parts[0].Trim();
                     string targetStateName = parts[1].Split(':')[0].Trim();
 
                     var split = parts[1].Split(':');
                     string @event = split.Length == 2 ? split[1].Trim() : null;
 
-                    State sourceState = FindStateByName(states, sourceStateName, qParrent.Peek());
-                    State targetState = FindStateByName(states, targetStateName, qParrent.Peek());
+                    State sourceState = FindStateByName(states, sourceStateName, qParrent.Peek(), Direction.Source);
+                    State targetState = FindStateByName(states, targetStateName, qParrent.Peek(), Direction.Target);
 
-                    if(sourceState == null)
+                    if (sourceState == null)
                     {
-                        sourceState = new State(++id, sourceStateName, qParrent.Peek());
+                        var type = "State";
+                        if (sourceStateName == "[*]")
+                            type = "Init";
+
+                        sourceState = new State(++id, sourceStateName, type, qParrent.Peek());
                         states.Add(sourceState);
                     }
 
                     if (targetState == null)
                     {
-                        targetState = new State(++id, targetStateName, qParrent.Peek());
+                        var type = "State";
+                        if (targetStateName == "[*]")
+                            type = "Init";
+
+                        targetState = new State(++id, targetStateName, type, qParrent.Peek());
                         states.Add(targetState);
                     }
 
@@ -101,16 +112,31 @@ public class PlantUmlAnalyzer
         return states;
     }
 
-    private State FindStateByName(List<State> states, string stateName, int currentaParrentId)
+
+
+    private State FindStateByName(List<State> states, string stateName, int currentaParrentId, Direction? direction = null)
     {
         if(stateName != "[*]") 
             return states.Find(state => state.Name == stateName);
         else
         {
-            if (currentaParrentId != -1)
-                return null;
+            if(direction == null 
+                || direction == Direction.Source)
+            {
+                if (currentaParrentId != -1)
+                    return null;
+                else
+                    return states.Find(state => state.Name == stateName && state.ParrentId == -1);
+            }
             else
-                return states.Find(state => state.Name == stateName && state.ParrentId == -1);
+            {
+                var exists = states.Where(state => state.ParrentId == currentaParrentId && state.Type == "Init")
+                    .OrderBy(state => state.Id).ToList();
+                if (exists.Count() <= 1)
+                    return null;
+                else
+                    return exists[1];
+            }
         }    
     }
 
@@ -119,27 +145,39 @@ public class PlantUmlAnalyzer
         return states.Find(state => state.Id == id);
     }
 }
-
+public enum Direction
+{
+    Source,
+    Target,
+}
 public class Program
 {
     public static void Main()
     {
         string plantUmlCode = @"
 @startuml
-state A {
-  state X {
-  }
-  state Y {
-  }
-}
- 
-state B {
-  state Z {
-  }
+scale 350 width
+[*] --> NotShooting
+
+state NotShooting {
+  [*] --> Idle
+  Idle --> Configuring : EvConfig
+  Configuring --> Idle : EvConfig
 }
 
-X --> Z
-Z --> Y
+state Configuring {
+  [*] --> NewValueSelection
+  NewValueSelection --> NewValuePreview : EvNewValue
+  NewValuePreview --> NewValueSelection : EvNewValueRejected
+  NewValuePreview --> NewValueSelection : EvNewValueSaved
+
+  state NewValuePreview {
+
+     State1 -> State2
+     State2 -> [*]
+  }
+
+}
 @enduml
         ";
 
